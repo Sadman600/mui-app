6026b1a9213e3393d378cb4bd96d0a91793e3201
Sadman600 <sadmansakib600@gmail.com>
2023-01-29 12:56:46 +0600

    Added Navbar

---
+++ componants/Footer.js
@@ -0,0 +1,14 @@
+import { Box, Typography } from '@mui/joy';
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <>
+            <Box>
+                <Typography>Footer</Typography>
+            </Box>
+        </>
+    );
+};
+
+export default Footer;
